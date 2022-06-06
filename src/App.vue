e2ce5d6474521bb0b165b48fec3fda83b671ec47
matthewmjm <matthewmjm@yahoo.com>
2022-06-05 19:16:45 -0500

    clean up boilerplate from vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
+/* eslint-disable prettier/prettier */
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Welcome to Vue" />
+	<h1>I'm a Teapot</h1>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
 	name: "App",
-	components: {
-		HelloWorld,
-	},
 };
 </script>
 
-<style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
-</style>
+<style></style>
