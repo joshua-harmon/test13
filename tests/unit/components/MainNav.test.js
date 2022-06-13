8bd7c00ef78e550fbfe58c44dd0ca1298846706c
matthewmjm <matthewmjm@yahoo.com>
2022-06-12 20:37:48 -0500

    second argument to Mount function

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -3,7 +3,13 @@ import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
 	it("displays company name", () => {
-		const wrapper = mount(MainNav);
+		const wrapper = mount(MainNav, {
+			data() {
+				return {
+					company: "Boogle Careers",
+				};
+			},
+		});
 		expect(wrapper.text()).toMatch("Boogle Careers");
 	});
 });
