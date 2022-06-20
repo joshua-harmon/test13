1c00d8ea1ac80fb4c663c5dd967e643603f2a062
matthewmjm <matthewmjm@yahoo.com>
2022-06-20 11:28:20 -0500

    intro to props using button text

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -2,13 +2,14 @@
 	<button
 		class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
 	>
-		Sign In
+		{{ text }}
 	</button>
 </template>
 
 <script>
 export default {
 	name: "ActionButton",
+	props: ["text"],
 	data() {
 		return {};
 	},
