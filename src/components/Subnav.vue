cd2b1dc92ac6c55ac297616e618930eac56b0ad5
matthewmjm <matthewmjm@yahoo.com>
2022-06-21 08:30:09 -0500

    Style sub nav component

--- src/components/Subnav.vue
+++ src/components/Subnav.vue
@@ -1,5 +1,7 @@
 <template>
-	<div>This is the Sub Nav</div>
+	<div class="w-full h-16 bg-white border-b border-solid border-brand-gray-1">
+		<div class="flex items-center h-full px-8">This is the Sub Nav</div>
+	</div>
 </template>
 
 <script>
